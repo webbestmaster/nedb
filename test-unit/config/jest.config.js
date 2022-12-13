2b7a2c8fd504ac6245779bd00211f0b78702ec84
dim <web.best.master@gmail.com>
2022-12-13 21:01:05 +0100

    tests

---
+++ test-unit/config/jest.config.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = {
+    moduleNameMapper: {
+        '^\\S+.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    },
+    modulePathIgnorePatterns: ['<rootDir>/tsc-check/'],
+    preset: 'ts-jest',
+    rootDir: '../../',
+    setupFilesAfterEnv: [],
+    testEnvironment: 'node',
+    // maxConcurrency: 1,
+    // maxWorkers: 1,
+    testTimeout: 1e3,
+};
